cfffabd8b0f19b0b409ac74075cd6f4cd78e1c89
kpolok <kamil.polok99@gmail.com>
2024-02-20 20:30:40 +0100

    Add zoom and pan functionaly to Chart canvas

--- app/ShaftDesigner/view/Chart/Chart.py
+++ app/ShaftDesigner/view/Chart/Chart.py
@@ -3,6 +3,8 @@ from matplotlib.backends.backend_qtagg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 
+import numpy as np
+
 class Chart(FigureCanvas):
     """
     A class representing a chart widget in a PyQt application.
@@ -46,6 +48,10 @@ class Chart(FigureCanvas):
         # Remove grid lines
         self.axes.grid(False)
 
+        # Add zoom and pan functionality
+        zoom_factory(self.axes)
+        pan_factory(self.axes)
+
     def _draw_axes_arrows(self):
         '''
         Draw axes on the canvas.
@@ -88,6 +94,71 @@ class Chart(FigureCanvas):
 
     def get_controls(self):
         return (self.axes, self.figure.canvas)
+
+def zoom_factory(axis, base_scale=1.5, min_zoom_range=0.1):
+    """
+    Returns zooming functionality to axis.
+    """
+    def zoom_fun(event):
+        """Zoom when scrolling."""
+        if event.inaxes == axis:
+            cur_xlim = axis.get_xlim()
+            cur_ylim = axis.get_ylim()
+            xdata, ydata = event.xdata, event.ydata
+
+            direction = np.sign(event.step)
+            scale_factor = np.power(base_scale, direction)
+
+            # Calculate proposed new limits
+            new_xlim = [xdata - (xdata - cur_xlim[0]) * scale_factor,
+                        xdata + (cur_xlim[1] - xdata) * scale_factor]
+            new_ylim = [ydata - (ydata - cur_ylim[0]) * scale_factor,
+                        ydata + (cur_ylim[1] - ydata) * scale_factor]
+
+            # Enforce zoom-in limit
+            if (new_xlim[1] - new_xlim[0]) < min_zoom_range or (new_ylim[1] - new_ylim[0]) < min_zoom_range:
+                return  # Prevent zooming in beyond the limit
+
+            # Apply the new limits
+            axis.set_xlim(new_xlim)
+            axis.set_ylim(new_ylim)
+
+            axis.figure.canvas.draw_idle()
+
+    fig = axis.get_figure()
+    fig.canvas.mpl_connect('scroll_event', zoom_fun)
+
+def pan_factory(axis):
+    """
+    Returns panning functionality to axis.
+    """
+    def on_press(event):
+        """Callback for mouse button press."""
+        if event.button == 3:  # Right mouse button
+            axis._pan_start = (event.xdata, event.ydata)
+
+    def on_release(event):
+        """Callback for mouse button release."""
+        axis._pan_start = None
+
+    def on_motion(event):
+        """Callback for mouse motion."""
+        if event.button == 3 and axis._pan_start is not None:  # Right mouse button
+            if event.xdata is not None and event.ydata is not None:
+                xdata_start, ydata_start = axis._pan_start
+                dx = event.xdata - xdata_start
+                dy = event.ydata - ydata_start
+                xlim = axis.get_xlim()
+                ylim = axis.get_ylim()
+                axis.set_xlim([xlim[0] - dx, xlim[1] - dx])
+                axis.set_ylim([ylim[0] - dy, ylim[1] - dy])
+                
+                axis.figure.canvas.draw_idle()
+
+    fig = axis.get_figure()
+    fig.canvas.mpl_connect('button_press_event', on_press)
+    fig.canvas.mpl_connect('button_release_event', on_release)
+    fig.canvas.mpl_connect('motion_notify_event', on_motion)
 
 class Toolbar(NavigationToolbar):
     """
