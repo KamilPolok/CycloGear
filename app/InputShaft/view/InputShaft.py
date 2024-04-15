1657a25b1ffcef3310dc42abb2fca73671822003
kpolok <kamil.polok99@gmail.com>
2024-04-15 13:51:55 +0200

    Organize function buttons in InputShaft

--- app/InputShaft/view/InputShaft.py
+++ app/InputShaft/view/InputShaft.py
@@ -1,5 +1,8 @@
-from PyQt6.QtCore import pyqtSignal
-from PyQt6.QtWidgets import QTabWidget, QWidget, QVBoxLayout, QPushButton
+from PyQt6.QtCore import pyqtSignal, QSize
+from PyQt6.QtGui import QIcon, QFont
+from PyQt6.QtWidgets import QTabWidget, QWidget, QHBoxLayout, QVBoxLayout, QPushButton
+
+from config import resource_path
 
 class InputShaft(QWidget):
     show_preview_signal = pyqtSignal()
@@ -18,14 +21,18 @@ class InputShaft(QWidget):
         """
         # Set layouts 
         self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.main_layout)
 
         self.tabs_section_layout = QVBoxLayout()
-        self.buttons_section_layout = QVBoxLayout()
+        self.tabs_section_layout.setContentsMargins(0, 0, 0, 0)
+        self.navigation_buttons_layout = QHBoxLayout()
+        self.navigation_buttons_layout.setContentsMargins(0, 0, 0, 0)
 
         self.main_layout.addLayout(self.tabs_section_layout)
-        self.main_layout.addLayout(self.buttons_section_layout)
+        self.main_layout.addLayout(self.navigation_buttons_layout)
 
+        # Set content
         self._init_tab_widget()
         self._init_function_buttons()
 
@@ -40,24 +47,73 @@ class InputShaft(QWidget):
 
     def _init_function_buttons(self):
         '''
-        Init next tab button and preview button.
+        Init previous tab button, next tab button and preview button.
         '''
-        # Add button for opening next tab 
-        self._next_tab_button = QPushButton('Dalej', self)
-        self._next_tab_button.clicked.connect(self.next_tab)
+        # Create button for moving to previous tab 
+        self._previous_tab_button = QPushButton(self)
+        self._previous_tab_button.setFixedSize(QSize(30, 30))
+        self._previous_tab_button.setIcon(QIcon(resource_path('icons//previous.png')))
+        self._previous_tab_button.setToolTip('Poprzednia zakładka')
+        self._previous_tab_button.setIconSize(QSize(25, 18))
+        self._previous_tab_button.clicked.connect(self._open_previous_tab)
 
-        # Add button for opening the shaft designer
+        # Create button for moving to next tab 
+        self._next_tab_button = QPushButton(self)
+        self._next_tab_button.setFixedSize(QSize(30, 30))
+        self._next_tab_button.setIcon(QIcon(resource_path('icons//next.png')))
+        self._next_tab_button.setToolTip('Następna zakładka')
+        self._next_tab_button.setIconSize(QSize(25, 18))
+        self._next_tab_button.clicked.connect(self._open_next_tab)
+
+        # Create button for opening the shaft preview
         self.preview_button = QPushButton('Podgląd', self)
+        self.preview_button.setToolTip('Otwórz podgląd wału')
+        self.preview_button.setFixedSize(QSize(100, 30))
+        font = QFont('Segoe UI', 12)
+        font.setBold(True)
+        self.preview_button.setFont(font)
 
-        self.buttons_section_layout.addWidget(self._next_tab_button)
-        self.buttons_section_layout.addWidget(self.preview_button)
+        distance_between_buttons = 10
+        self.navigation_buttons_layout.setSpacing(distance_between_buttons)
 
-    def _on_tab_change(self, tab_index):
+        self.navigation_buttons_layout.addStretch(1)
+        self.navigation_buttons_layout.addWidget(self._previous_tab_button)
+        self.navigation_buttons_layout.addWidget(self.preview_button)
+        self.navigation_buttons_layout.addWidget(self._next_tab_button)
+        self.navigation_buttons_layout.addStretch(1)
+
+    def _open_next_tab(self):
+        """
+        Move to the next tab.
+        """
+        next_index = self._tab_widget.currentIndex() + 1
+
+        if next_index < self._tab_widget.count():
+            self._tab_widget.setTabEnabled(next_index, True)
+            self._tab_widget.setCurrentIndex(next_index)
+    
+    def _open_previous_tab(self):
+        """
+        Move to the previous tab.
+        """
+        previous_index = self._tab_widget.currentIndex() - 1
+
+        if previous_index >= 0:
+            self._tab_widget.setCurrentIndex(previous_index)
+
+    def _on_tab_change(self, tab_index = 0):
         """
         Perform actions after a change of tabs.
         """
+        # Disable the next tab button if the current tab is the last one
         if tab_index == self._tab_widget.count() - 1:
-            self._next_tab_button.setDisabled(True)
+            self._next_tab_button.setEnabled(False)
+
+        # Toggle the visibility of the previous tab button
+        if tab_index == 0:
+            self._previous_tab_button.setEnabled(False)
+        else:
+            self._previous_tab_button.setEnabled(True)
 
     def init_tabs(self, tabs, tabs_titles):
         """
@@ -70,16 +126,6 @@ class InputShaft(QWidget):
         # Disable all tabs except the first one
         for i in range(1, self._tab_widget.count()):
             self._tab_widget.setTabEnabled(i, False)
-
-    def next_tab(self):
-        """
-        Move to the next tab.
-        """
-        next_index = self._tab_widget.currentIndex() + 1
-
-        if next_index < self._tab_widget.count():
-            self._tab_widget.setTabEnabled(next_index, True)
-            self._tab_widget.setCurrentIndex(next_index)
         
     def update_access_to_next_tabs(self, enable_next_tab_button, disable_next_tabs):
         """
